b59987176693d64d5125d526028cae3f99d95261
Bob Pace <bob.pace@gmail.com>
2011-12-06 09:06:27 -0700

    getting click set up

--- test.js
+++ test.js
@@ -1,14 +1,29 @@
+var ImageHelper = function(src) {
+  this.path = src;
+};
+
+ImageHelper.prototype.selected = function() {
+  alert('selected');
+};
+
 $(function() {
+
     var viewModel = {
       images: ko.observableArray([
-        {path: 'Desert.jpg'},
-        {path: 'Jellyfish.jpg'},
-        {path: 'Tulips.jpg'},
-        {path: 'test.jpg'},
-      ])
+        new ImageHelper('Desert.jpg'),
+        new ImageHelper('Jellyfish.jpg'),
+        new ImageHelper('Tulips.jpg'),
+        new ImageHelper('Hydrangeas.jpg'),
+        new ImageHelper('Koala.jpg'),
+        new ImageHelper('Penguins.jpg'),
+        new ImageHelper('Lighthouse.jpg'),
+        new ImageHelper('Chrysanthemum.jpg')
+      ]),
+      selectedImage: ko.observable('')
     };
 
     ko.applyBindings(viewModel);
 
-    $(".thumbs").thumbs();
+    $(".thumbs").thumbs({strip:false});
+    $(".viewport").canvasHelper({image: "./images/test.jpg"});
 });
