19e8c27820e1496d5f21d6caf521d4042531a111
Bob Pace <bob.pace@gmail.com>
2011-12-16 00:14:19 -0700

    trying out new ideas

---
+++ canvas.js
@@ -0,0 +1,137 @@
+(function(){
+
+  var images = [
+        'Chrysanthemum.jpg',
+        'Jellyfish.jpg',
+        'Desert.jpg',
+        'Hydrangeas.jpg',
+        'Jellyfish.jpg',
+        'Koala.jpg',
+        'Lighthouse.jpg',
+        'Penguins.jpg',
+        'test.jpg',
+        'Tulips.jpg',
+      ],
+      items = [
+        {},
+        {},
+        {},
+        {},
+        {},
+      ];
+
+  
+  loadImages({
+      target: '#design-images',
+      images: images
+    }
+  );
+
+  loadItems({
+      target: '#design-items',
+      items: items
+    }
+  );
+
+  function loadItems(options) {
+    var settings = $.extend({}, options),
+        items = settings['items'];
+
+    $.each(items, function(index, value) {
+      var target = $(settings['target']);
+      target.append($("<li>").data('novella-canvas', new NovellaCanvas({
+        overlaySrc: './images/previewWindow.png'
+      })));
+    });
+  }
+
+  function NovellaCanvas(options) {
+    var settings = $.extend({}, options),
+        overlaySrc = settings['overlaySrc'],
+        overlayImage;
+
+    if (overlaySrc) {
+      overlayImage = new Image();
+      overlayImage.onload = function() {
+      };
+      overlayImage.src = this.overlaySrc = overlaySrc;
+    }
+
+    this.draw = function(element) {
+      var canvas = $('<canvas>')[0],
+          context = canvas.getContext('2d');
+
+      if (overlayImage) {
+        context.drawImage(overlayImage, 0, 0);
+      }
+      element.html(canvas);
+    };
+  }
+
+  function loadImages(options) {
+    var defaults = {width: 75, height: 75},
+        settings = $.extend({}, defaults, options),
+        images = settings['images'];
+
+    $.each(images, function(index, value) {
+      var image = new Image(),
+          target = $(settings['target']);
+      image.src = "./images/" + value;
+      image.onload = function() {
+        var size = settings,
+            newSize = determineSize(size, this),
+            margin = determineMargin(size, newSize),
+            $this = $(this);
+
+        this.width = newSize.width;
+        this.height = newSize.height;
+        $this.css({
+          margin: margin
+        });
+        $this.addClass('unselectable');
+      };
+
+      target.append($("<li>").css({width: settings.width, height: settings.height}).append(image));
+    });
+  }
+
+  function determineMargin(containerSize, actualSize) {
+    var topMargin = 0,
+        sideMargin = 0;
+
+    if (containerSize.width > actualSize.width) {
+      sideMargin = (containerSize.width - actualSize.width) / 2;
+    }
+    else if (containerSize.height > actualSize.height) {
+      topMargin = (containerSize.height - actualSize.height) / 2;
+    }
+    return topMargin + "px " + sideMargin + "px";
+  }
+
+
+  function determineSize(boundingBox, sourceBox) {
+    var max_width = boundingBox.width,
+        max_height = boundingBox.height,
+        width = sourceBox.width,
+        height = sourceBox.height;
+
+    if (width > height) {
+      if (width > max_width) {
+        height *= max_width / width;
+        width = max_width;
+      }
+    }
+    else {
+      if (height > max_height) {
+        width *= max_height / height;
+        height = max_height;
+      }
+    }
+
+    return {
+      width: width,
+      height: height
+    };
+  }
+
+}())
--- test.js
+++ test.js
@@ -1,52 +1,51 @@
+
 $(function() {
-  var ImageHelper = function(src) {
-        this.path = this.imagePath + src;
-      }, 
-      images,
-      viewModel;
-
-    ImageHelper.prototype.imagePath = './images/';
-    ImageHelper.prototype.selected = function() {
-      viewModel.selectedImage(this);
+var ImageHelper = function(src) {
+      this.path = this.imagePath + src;
     };
 
-    images = [
-      new ImageHelper('test.jpg'),
-      new ImageHelper('Jellyfish.jpg'),
-      new ImageHelper('Tulips.jpg'),
-      new ImageHelper('Hydrangeas.jpg'),
-      new ImageHelper('Koala.jpg'),
-      new ImageHelper('Penguins.jpg'),
-      new ImageHelper('Lighthouse.jpg'),
-      new ImageHelper('Chrysanthemum.jpg'),
-      new ImageHelper('Jellyfish.jpg'),
-    ];
+ImageHelper.prototype = {
+  imagePath: './images/',
+  selected: function() {
+    viewModel.selectedImage(this);
+  }
+};
+  var images = [
+        new ImageHelper('test.jpg'),
+        new ImageHelper('Jellyfish.jpg'),
+        new ImageHelper('Tulips.jpg'),
+        new ImageHelper('Hydrangeas.jpg'),
+        new ImageHelper('Koala.jpg'),
+        new ImageHelper('Penguins.jpg'),
+        new ImageHelper('Lighthouse.jpg'),
+        new ImageHelper('Chrysanthemum.jpg'),
+        new ImageHelper('Jellyfish.jpg'),
+      ],
+      viewModel = {
+        images: images,
+        selectedImage: ko.observable('')
+      };
 
-    viewModel = {
-      images: images,
-      selectedImage: ko.observable('')
-    };
+  viewModel.selectedImage.subscribe(function(data) {
+    var imageViewport = $("#image-viewport"),
+        imageCanvas = imageViewport.find('canvas')[0]
+        novellaViewport = $("#novella-viewport"),
+        novellaCanvas = novellaViewport.find('canvas')[0],
+        imageCanvasHelper = imageViewport.canvasHelper({
+          image: data.path,
+          drag: function(position) {
+            novellaCanvasHelper.redraw(position);
+          }
+        }),
+        novellaCanvasHelper = novellaViewport.canvasHelper({
+          image: './images/previewWindow.png',
+          innerCanvas: imageCanvas
+        });
+  });
 
-    viewModel.selectedImage.subscribe(function(data) {
-      var imageViewport = $("#image-viewport"),
-          imageCanvas = imageViewport.find('canvas')[0]
-          novellaViewport = $("#novella-viewport"),
-          novellaCanvas = novellaViewport.find('canvas')[0],
-          imageCanvasHelper = imageViewport.canvasHelper({
-            image: data.path,
-            drag: function(position) {
-              novellaCanvasHelper.redraw(position);
-            }
-          }),
-          novellaCanvasHelper = novellaViewport.canvasHelper({
-            image: './images/previewWindow.png',
-            innerCanvas: imageCanvas
-          });
-    });
+  viewModel.selectedImage(images[0])
 
-    viewModel.selectedImage(images[0])
+  ko.applyBindings(viewModel);
 
-    ko.applyBindings(viewModel);
-
-    $(".thumbs").thumbs();
+  $(".thumbs").thumbs();
 });
